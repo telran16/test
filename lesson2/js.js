4f29e814d472058a3b153ad63bb60a173fcada4c
Kostya <telrun16@gmail.com>
2019-06-16 01:59:01 +0300

    This first commit

---
+++ lesson2/js.js
@@ -0,0 +1,26 @@
+let money, time;
+
+    money = prompt("budget in month?", 1000),
+    time = prompt("input date", '');
+
+
+let appData= {
+    budget: +money,
+    timeData: time,
+    expenses: {
+        question1: {},
+        question2: {}
+    },
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}
+
+appData.expenses.question1.what = prompt("What need to buy?", 'pie');
+appData.expenses.question1.price = +prompt("How mutch it cost?", '10');
+appData.expenses.question2.what = prompt("What need to buy?", '');
+appData.expenses.question2.price = +prompt("How mutch it cost?", '');
+
+alert('budjet for a day: ' + (appData.budget - appData.expenses.question1.price) / 30);
+
+console.log(appData);
